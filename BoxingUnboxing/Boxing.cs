3f7595c04c4792a0b3821065ff707d12a505d07f
Sampadahakke <sampadahakke102@gmail.com>
2022-12-04 14:04:19 +0530

    [Sampada]Adding project on boxing unboxing

---
+++ BoxingUnboxing/Boxing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingUnboxing
+{
+    public class Boxing
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------Boxing------------");
+            int num = 20;
+            object value = num;
+            Console.WriteLine("The value num in object = " + value);
+            Console.WriteLine("The value num in int = " + num);
+
+            Console.WriteLine("-----------Unboxing------------");
+            Unboxing.Unbox();
+            Console.ReadLine();
+        }
+      
+    }
+}
---
+++ BoxingUnboxing/Unboxing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingUnboxing
+{
+    public class Unboxing
+    {
+        public static void Unbox()
+        {
+            int num1 = 20;
+            object value = num1;
+            int num2 = (int)value;
+            Console.WriteLine("The value num in object = " + value);
+            Console.WriteLine("The value num2 in int = " + num2 );
+            Console.ReadKey();
+        }
+    }
+}
